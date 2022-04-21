01b64598ec88168b3f543d7ba919e509cc3de899
dom2code <dom2code@gmail.com>
2022-04-21 17:42:32 +0100

    email for password reset

--- adwebsite_project/settings.py
+++ adwebsite_project/settings.py
@@ -157,6 +157,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+EMAIL_HOST_USER = 'x21168083.test@gmail.com'
+EMAIL_HOST_PASSWORD = '7305608524'
 
